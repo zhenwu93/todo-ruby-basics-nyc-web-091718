f0ee69ff2227ee68986d1bd13c0bfe7ab98314f1
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-10 02:43:58 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -18,8 +18,8 @@ def return_a_value(phrase = "Nice")
   return "#{phrase}"
 end
 
-def last_evaluated_value
-
+def last_evaluated_value(phrase = "expert")
+  return "#{phrase}"
 end
 
 def pizza_party
